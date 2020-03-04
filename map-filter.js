ff7a37b3498ed87902f932dc0caa1f7693eb5268
jimtanvir <tanvirrahman117@gmail.com>
2020-03-04 19:11:03 +0600

    added

---
+++ map-filter.js
@@ -0,0 +1,19 @@
+const numbers = [3,4,5,6,7];
+// const output = [];
+
+// for(let i = 0;i <numbers.length;i++){
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+
+
+// const result = numbers.map(element => element * element)
+
+// const result = numbers.filter(x => x>5)
+
+
+const result = numbers.find(x => x>5)
+console.log(result);
+
+
