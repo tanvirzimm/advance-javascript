3b7b0a9876402e4bf7172e57edf1eb4696161900
jimtanvir <tanvirrahman117@gmail.com>
2020-03-04 18:16:04 +0600

    commited

---
+++ truthy-falsy-value.js
@@ -0,0 +1,14 @@
+//falsy values
+//0 , "" , false , null , undefined , NaN
+
+let name = 0;
+
+if(name || name ==0){
+    console.log("condition is true");
+    
+}
+
+else{
+    console.log("condition is false");
+    
+}
